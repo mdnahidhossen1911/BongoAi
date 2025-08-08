46d001a2acb7075a4dde94391e773a29e05971b0
mdnah <md.nahidhossen1911@gmail.com>
2025-08-08 08:37:45 +0600

    text copy

--- lib/utils/components/chat_bubble.dart
+++ lib/utils/components/chat_bubble.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../models/message.dart';
 
@@ -12,35 +13,51 @@ class ChatBubble extends StatelessWidget {
     if (message.role == 'user') {
       return Align(
         alignment: Alignment.centerRight,
-        child: Container(
-          margin: EdgeInsets.symmetric(vertical: 16),
-          padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-          constraints: BoxConstraints(
-            maxWidth: MediaQuery.of(context).size.width * 0.7,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.grey.shade100,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(18),
-              topRight: Radius.circular(18),
-              bottomLeft: Radius.circular(18),
+        child: GestureDetector(
+          onLongPress: () {
+            Clipboard.setData(ClipboardData(text: message.content));
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Message copied to clipboard')),
+            );
+          },
+          child: Container(
+            margin: EdgeInsets.symmetric(vertical: 16),
+            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width * 0.7,
             ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.05),
-                blurRadius: 4,
-                offset: Offset(0, 2),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade100,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(18),
+                topRight: Radius.circular(18),
+                bottomLeft: Radius.circular(18),
               ),
-            ],
-          ),
-          child: Text(
-            message.content,
-            style: TextStyle(color: Colors.black87, fontSize: 16),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.05),
+                  blurRadius: 4,
+                  offset: Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Text(
+              message.content,
+              style: TextStyle(color: Colors.black87, fontSize: 16),
+            ),
           ),
         ),
       );
     } else if (message.role == 'assistant') {
-      return Text(message.content);
+      return GestureDetector(
+        onLongPress: () {
+          Clipboard.setData(ClipboardData(text: message.content));
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Message copied to clipboard')),
+          );
+        },
+        child: Text(message.content),
+      );
     } else {
       return SizedBox();
     }
